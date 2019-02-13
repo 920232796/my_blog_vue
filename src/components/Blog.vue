f3ba577b1ae9a54a8c6c376b2ec28933deab72c8
18340097191 <920232796@qq.com>
2019-02-13 14:12:37 +0800

    start

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="container">
       <div class="navigation">
-        <img src="@/assets/ww.jpg" alt="" height="120px" class="navigation_image">
+        <!--<img src="@/assets/ww.jpg" alt="" height="120px" class="navigation_image">-->
+        <img src="@/assets/logo-xing.png" alt="" height="40px" class="navigation_image">
+        <div class="navigation_text">安 。</div>
+        <div class="navigation_class"><span class="single_class">Python</span>
+          <span class="single_class">Java</span><span class="single_class">深度学习</span>
+          <span class="single_class">前端</span><span class="single_class">服务器</span>
+          <span class="single_class">关于</span>
+          <!--<span class="single_class"><img src="@/assets/search.png" height="30px" alt=""></span>-->
+        </div>
       </div>
       <div class="content">
         <div class="blog">
-          <div class="block">
+          <div class="blog_block">
             <img src="@/assets/ww.jpg" alt="" width="150px" height="80px" class="blog_image">
             <div class="text_content">
               <div class="title">
@@ -17,8 +25,31 @@
               <div class="description">AC自动机中，转移的最小单位是一个字符。也就是说，匹配后只能移动一个字符，复杂度是线性的$O(n)$。然而线性并非最快，Boyer-Moore算法在匹配后可以跳过多个字符，...</div>
             </div>
           </div>
+
+
+          <div class="block">
+            <el-pagination
+              background
+              @current-change="handleCurrentChange"
+              :page-size="pageSize"
+              layout="prev, pager, next"
+              :total="totalNumber">
+            </el-pagination>
+          </div>
+
+
         </div>
+
+        <div class = "search_input_btn">
+          <el-input v-model="input" placeholder="请输入内容" class="search_input"></el-input>
+          <el-button @click="handleBtnClick" size="medium" type="primary" class="search_btn">搜索</el-button>
+        </div>
+
+
+
+
       </div>
+
 
     </div>
 </template>
@@ -27,7 +58,14 @@
     export default {
         name: '',
         data () {
-            return {}
+            return {
+              input: "",
+              start: 0,
+              limit: 5,
+              resultList: [],
+              totalNumber: 0,
+              pageSize: 5
+            }
         }
     }
 </script>
@@ -43,30 +81,69 @@
     width: 100%;
     height: 120px;
     background-color: #ffffff;
-    box-shadow:0 0 2px #000 ;
+    box-shadow:0 0 2px #666 ;
   }
   .navigation_image {
-    margin-left: 100px;
+    margin-left: 140px;
+    margin-top: 50px;
+    float: left;
+  }
+  .navigation_text {
+    width: 100px;
+    height: 40px;
+    margin-top: 50px;
+    margin-left: 30px;
+    padding-left: 30px;
+    border-left: 2px solid #D2E0E6;
+    font-family: "Microsoft YaHei UI";
+    font-weight: bold;
+    font-size: 25px;
+    float: left;
+    color: #666;
+    /*border:1px solid red;*/
+  }
+  .navigation_class {
+    width: 650px;
+    height: 40px;
+    /*border:1px solid red;*/
+    float: right;
+    margin-top: 50px;
+    font-size: 21px;
+    margin-right: 60px;
+  }
+  .single_class {
+    width: 60px;
+    height: 40px;
+    margin-left: 30px;
+    color: #666;
+  }
+  .single_class:hover {
+    color: #428bca;
   }
   .content{
     height: 700px;
     width: 100%;
     margin-top: 5px;
+    /*float: left;*/
     /*border: 1px solid red;*/
   }
   .blog {
     width: 800px;
-    height: 650px;
+    height: 680px;
     margin-top: 10px;
     margin-left: 8%;
-    box-shadow:0 0 2px #000 ;
+    float: left;
+    box-shadow:0 0 2px #666 ;
     background-color: #ffffff;
   }
-  .block {
-    width: 750px;
-    height: 120px;
+  .blog_block {
+    width: 800px;
+    height: 140px;
     border-bottom: 1px solid #D2E0E6;
     /*border: 1px solid blue;*/
+  }
+  .blog_block:hover {
+    background-color: #eaeaea;
   }
   .blog_image {
     margin: 20px 20px;
@@ -77,6 +154,7 @@
     width: 520px ;
     height: 110px;
     float: left;
+    color: #555;
     margin-top: 6px;
     /*border: 1px solid yellow;*/
   }
@@ -84,7 +162,12 @@
     width: 400px;
     height: 30px;
     font-size: 25px;
+    color: #555;
+    /*font-family: Helvetica, Tahoma;*/
     /*border: 1px solid red;*/
+  }
+  .title:hover {
+    color: #428bca;
   }
   .middle {
     width: 400px;
@@ -109,5 +192,35 @@
     /*border: 1px solid goldenrod;*/
     overflow: hidden;
     font-size: 13.5px;
+  }
+
+
+
+  .search_input_btn{
+    float: left;
+    width: 550px;
+    height: 130px;
+    /*border: 1px solid red;*/
+    margin-left: 25px;
+    margin-top: 10px;
+  }
+  .search_btn {
+    float: left;
+    width: 120px;
+    height: 40px;
+    /*border: 1px solid black;*/
+  }
+  .search_input {
+    float: left;
+    width: 400px;
+    height: 115px;
+  }
+
+  .block {
+    float: bottom;
+    width: 300px;
+    margin: 20px auto ;
+    /*border:1px solid red;*/
+    height: 60px;
   }
 </style>
