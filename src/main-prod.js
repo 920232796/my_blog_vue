13cefa52b42f6c2d70b7a6fac85b2dc93ab51d5f
xingzhaohu <920232796@qq.com>
2020-09-09 21:57:30 +0800

    修复一些bug

--- src/main-prod.js
+++ src/main-prod.js
@@ -7,7 +7,7 @@ import router from './router'
 // import 'element-ui/lib/theme-chalk/index.css';
 
 import mavonEditor from 'mavon-editor'
-// import 'mavon-editor/dist/css/index.css'
+import 'mavon-editor/dist/css/index.css'
 
 Vue.use(mavonEditor)
 
